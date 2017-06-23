6eec7fea1f3091268bf7ac54754018a7b7c065ec
sgriff <seanmigriffin@gmail.com>
2017-06-23 16:46:23 -0400

    first commit

---
+++ gmSentiment.py
@@ -0,0 +1,46 @@
+import tweepy
+import datetime
+from textblob import TextBlob
+
+
+# Authenticate twitter API
+consumer_key = 'C5jmJBijqEanOCOvonuJGYrhw'
+consumer_secret = 'UqmXukdeYoc4YegC8UMDSJzOyAOE2AXBukmY1jR9ShxomF3nFg'
+
+access_token = '852256333705474052-Oy4Nfb9mOKyCJdJxw6lCRjU3mbzoqSv'
+access_token_secret = 'i3SwzWw0wCCl6oMgAykQ678I6hLRChTSfMPxEdtDRAmeN'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# List of GM names
+gm_names = ['Ducks Murray', 'Chayka', 'Sweeney', 'Botterill', 'Treliving', 'Ron Francis', 'Bowman', 'Sakic', 'Kekalainen', 'Nill', 'Ken Holland', 'Chiarelli', 'Tallon', 'Rob Blake', 'Chuck Fletcher', 'Bergevin', 'Poile', 'Shero', 'Garth Snow', 'Gorton', 'Dorion', 'Hextall', 'Rutherford', 'Doug Wilson', 'Doug Armstrong', 'Yzerman', 'Lamoriello', 'Jim Benning', 'McPhee', 'Brian MacLellan', 'Cheveldayoff']
+
+# Draft dates to present
+exp_date = "2017-06-21"
+curr_date = str(datetime.date.today())
+
+
+# Function to label sentiment
+def label(analysis, threshold=0):
+    if analysis.sentiment[0] > threshold:
+        return 'Positive'
+    else:
+        return 'Negative'
+
+
+# Retreive tweets and save them to csv
+for gm in gm_names:
+    gm_polarities = []
+    # Get the tweets about GMs since the expansion draft
+    gm_tweets = api.search(q=[gm], count=500, since=exp_date, until=curr_date)
+    # Save tweets in csv
+    with open('%s_tweets.csv' % gm, 'w') as gm_file:
+        gm_file.write('tweet, sentiment, polarity\n')
+        for tweet in gm_tweets:
+            analysis = TextBlob(str(tweet))
+            # Get label according to sentiment
+            gm_polarities.append(analysis.sentiment.polarity)
+            gm_file.write('%s,%s,%s\n' % (tweet.text.encode('utf8'), analysis.sentiment.polarity, label(analysis)))
